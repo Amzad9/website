b78bf2a93cecd3c33ff3e957b989a39f00cb9d9f
mohd shadab saifi <shadabsaifi749@gmail.com>
2025-02-23 01:02:56 +0530

    add header

---
+++ src/components/header/ContactUsModal.jsx
@@ -0,0 +1,118 @@
+// 'use client';
+
+// import ModalPortal from '@/common/ModalPortal'
+// import React, { useState } from 'react'
+// import styles from "@/components/header/Header.module.scss";
+// import Image from 'next/image';
+// import localFont from 'next/font/local';
+// // import Button from '@/common/Button';
+
+// // // Font files can be colocated inside of `app`
+// // const BankGothic = localFont({
+// //     src: "../../../public/font/BankGothic_Md_BT.ttf",
+// //     display: "swap",
+// // });
+
+// const ContactUsModal = ({ isModal, onClose }) => {
+
+
+//     const [formData, setFormData] = useState({
+//         name: '',
+//         email: '',
+//         phone: '',
+//         walletLink: '',
+//         file: null,
+//     });
+
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData((prevData) => ({
+//             ...prevData,
+//             [name]: value,
+//         }));
+//     };
+
+//     const handleFileChange = (e) => {
+//         setFormData((prevData) => ({
+//             ...prevData,
+//             file: e.target.files[0],
+//         }));
+//     };
+
+
+//     return (
+//         <ModalPortal childClassName={styles.contact_modal_wrapper} show={isModal} onClose={onClose}>
+//             <div className={styles.header_wrap}>
+//                 <h1 className={`${styles.heading}`}>اتصل بنا</h1>
+//                 <button className={styles.close_btn} onClick={onClose}>
+//                     <Image src="https://loopwebsite.s3.ap-south-1.amazonaws.com/basil_cross-solid.svg" alt="" width="28" height="28" />
+//                 </button>
+//             </div>
+
+//             <div className={styles.modal_Body}>
+//                 <div className={styles.form_wrapper}>
+//                     <input
+//                         type="text"
+//                         name="name"
+//                         placeholder="أدخل الاسم"
+//                         value={formData.name}
+//                         onChange={handleChange}
+//                         className={styles.form_input}
+//                     />
+//                 </div>
+
+//                 <div className={styles.form_wrapper}>
+//                     <input
+//                         type="email"
+//                         name="email"
+//                         placeholder="أدخل البريد الإلكتروني"
+//                         value={formData.email}
+//                         onChange={handleChange}
+//                         className={styles.form_input}
+//                     />
+//                 </div>
+
+//                 <div className={styles.form_wrapper}>
+//                     <input
+//                         type="number"
+//                         name="phone"
+//                         placeholder="أدخل رقم الجوال"
+//                         value={formData.phone}
+//                         onChange={handleChange}
+//                         className={styles.form_input}
+//                     />
+//                 </div>
+
+//                 <div className={styles.form_wrapper}>
+//                     <input
+//                         type="text"
+//                         name="walletLink"
+//                         placeholder="رابط المحفظة"
+//                         value={formData.walletLink}
+//                         onChange={handleChange}
+//                         className={styles.form_input}
+//                     />
+//                 </div>
+
+//                 <div className={styles.form_wrapper}>
+//                     <textarea
+//                         type="text"
+//                         name="walletLink"
+//                         placeholder="اكتب رسالتك"
+//                         row="10"
+//                         // value={formData.walletLink}
+//                         // onChange={handleChange}
+//                         className={styles.form_inputarea}
+//                     />
+//                 </div>
+
+//                 <div className={styles.btn_group}>
+//                     <Button type="button" className={styles.apply_btn}>إرسال</Button>
+//                     <Button type="button" className={styles.close_btn} onClick={onClose}>يغلق</Button>
+//                 </div>
+//             </div>
+//         </ModalPortal>
+//     )
+// }
+
+// export default ContactUsModal
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from "react";
+import styles from "@/components/header/Header.module.scss";
+import Logo from "@/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+// import localFont from "next/font/local";
+// import ContactUsModal from "./ContactUsModal";
+
+// Font files can be colocated inside of `app`
+// const BankGothic = localFont({
+//   src: "../../../public/font/BankGothic_Md_BT.ttf",
+//   display: "swap",
+// });
+
+const Header = ({ isOpenNavbar, setIsOpenNavbar }) => {
+  // const router = useRouter()
+  const pathname = usePathname();
+  const [scrolled, setScrolled] = useState(false);
+  const [isModal, setIsModal] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // const handleContactUs = () => {
+  //   // router.push('/contact-us', { scroll: false })
+  //   window.open("/contact-us", "_blank", "noopener,noreferrer");
+  // };
+
+  const handleNavbar = () => {
+    setIsOpenNavbar(!isOpenNavbar);
+  };
+
+  const handleContactUS = () => {
+    setIsModal(true);
+  };
+  const handleContactUSClose = () => {
+    setIsModal(false);
+  };
+
+  const isRoutes = [
+    "/service",
+    "/AI-Ml",
+    "/web3",
+    "/blockchain",
+    "/software",
+    "/internet-of-things",
+  ];
+
+  return (
+    <>
+      <div className="container">
+        <header
+          className={`${styles.headerWrapper} ${
+            scrolled ? styles.stickyActive : ""
+          }`}
+        >
+          <div className={styles.header}>
+            <Link href="/" className={styles.logo}>
+              <Image
+                src={Logo}
+                alt="Logo"
+                className={styles.logoImage}
+                width={200}
+                // height={63}
+              />
+              {/* Weblibron */}
+            </Link>
+
+            <nav className={styles.menu}>
+              {[
+                "Home",
+                "About us",
+                "Services",
+                "Technology",
+                "Team",
+                "Clients",
+              ].map((item) => (
+                <Link
+                  key={item}
+                  href={`#${item.toLowerCase()}`}
+                  className={`${styles.menuItem} text-black ${
+                    pathname === "/solution" ? styles.active : ""
+                  }`}
+                >
+                  {item}
+                </Link>
+              ))}
+            </nav>
+
+            <div className={styles.group_btn}>
+              <button
+                className={`${styles.contactButton}`}
+                onClick={handleContactUS}
+              >
+                Contact Us
+              </button>
+              <button className={styles.humberger} onClick={handleNavbar}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+          </div>
+        </header>
+      </div>
+
+      {/* <ContactUsModal isModal={isModal} onClose={handleContactUSClose} /> */}
+    </>
+  );
+};
+
+export default Header;
