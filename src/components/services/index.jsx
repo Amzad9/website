4c8d9ecec5048ab038f212ee3482a75fefeebcd9
mohd shadab saifi <shadabsaifi749@gmail.com>
2025-02-24 02:36:36 +0530

    add services

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -26,7 +26,7 @@ export default function Services() {
             </h1>
             <span className="absolute h-5 w-full rounded-xl bg-slate-50 opacity-50 blur-lg top-10 backdrop-blur-sm  left-0 transform -translate-y-1/2"></span>
           </div>
-          <div className="flex flex-col md:flex-row justify-center gap-4 mb-12">
+          <div className="flex flex-col flex-wrap w-1/2 mx-auto md:flex-row justify-center gap-4 mb-12">
             <button className="bg-transparent text-white border-2 border-white px-6 py-1.5 rounded-full  hover:text-primary hover:border-primary transition font-poppins font-medium">
               Web Applications
             </button>
@@ -34,7 +34,13 @@ export default function Services() {
               Mobile Applications
             </button>
             <button className="bg-transparent text-white border-2 border-white px-6 py-1.5 rounded-full font-medium hover:text-primary hover:border-primary transition font-poppins">
+              U/i/Ux Dessign
+            </button>
+            <button className="bg-transparent text-white border-2 border-white px-6 py-1.5 rounded-full font-medium hover:text-primary hover:border-primary transition font-poppins">
               Business Platforms
+            </button>
+            <button className="bg-transparent text-white border-2 border-white px-6 py-1.5 rounded-full font-medium hover:text-primary hover:border-primary transition font-poppins">
+              Software Support
             </button>
           </div>
           <div className="mb-6">
