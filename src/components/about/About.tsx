f6baf88edc9e55a19639fc0d96a5520b06e67b1e
amzad949 <amzad.khan@ftachdata.com>
2025-02-28 23:47:37 +0530

    design about us page

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,8 +1,19 @@
 const AboutUs = () => {
   return (
-    <>
-      <div>About</div>
-    </>
+    <section className="py-16 bg-black">
+  <div className="container md:container mx-auto">
+      <div className="flex justify-center">
+         <div className="w-full md:w-3/3">
+<h2 className="font-inter text-5xl font-semibold mb-4 text-center text-white">Hi. We&apos;re weblibron</h2>
+           <p className="text-lg font-poppins mb-3 text-center text-gray-500"> A team of passionate <span className="text-white">designers</span> and <span className="text-white">developers</span> specializing in <span className="text-white">web</span> and <span className="text-white">app design</span> and <span className="text-white">development</span>. We believe that combining <span className="text-white">stunning design</span> with robust development is the foundation of exceptional <span className="text-white">digital experiences</span>.
+</p>
+<p className="text-lg font-poppins text-center text-gray-500">Our mission is to <span className="text-white">empower businesses with</span> <span className="text-white">tailor-made web and app solutions</span> that <span className="text-white">stand out</span> and <span className="text-white">perform flawlessly</span>. We focus on crafting <span className="text-white">user-centric designs and seamless functionality</span> to <span className="text-white">boost engagement, optimize performance</span>, and <span className="text-white">elevate your brand.</span> Whether you need a <span className="text-white">website, a mobile app</span>, or a <span className="text-white">full digital transformation</span>, <span className="text-primary">weblibron</span> is here to <span className="text-white">design and develop</span> your vision into <span className="text-white">reality</span>.</p>
+         </div>
+
+      </div>
+
+</div>
+    </section>
   );
 };
 
