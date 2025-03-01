8ada13ab6a2742f5ca8e9ba5859d9b652b6fdd02
mohd shadab saifi <shadabsaifi749@gmail.com>
2025-03-01 18:17:15 +0530

    add footer

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,23 +1,25 @@
 import { PlusCircle } from "lucide-react";
 const Contact = () => {
-return(
-   <section className="text-black py-40 relative z-10">
-        <div className="container mx-auto px-6 text-start">
-          <div className="relative">
-            <h1 className="text-7xl flex item-center text-white relative z-10 font-semibold mb-12 font-poppins">
-              Let’s <span className="text-primary italic font-inter mx-3">Build</span>{" "}
-              Together! <button className="ms-5 bg-transparant border-2 text-white hover:border-green-600 text-black font-semibold w-12 h-12 flex justify-center items-center rounded-full">
-            <PlusCircle />
-          </button>
-            </h1>
-            <span className="absolute h-5 w-[60%] rounded-xl bg-slate-50 opacity-50 blur-lg top-10 backdrop-blur-sm  left-0 transform -translate-y-1/2"></span>
-          </div>
-          <p className="text-xl mb-8 text-white">
-Developing reliable mobile and web experiences.          </p>
-         
+  return (
+    <section className="gradient-section text-black py-5 relative z-10">
+      <div className="container mx-auto px-6 text-start">
+        <div className="relative">
+          <h1 className="text-7xl flex item-center text-white relative z-10 font-semibold mb-12 font-poppins">
+            Let’s{" "}
+            <span className="text-primary italic font-inter mx-3">Build</span>{" "}
+            Together!{" "}
+            <button className="ms-5 bg-transparant border-2 text-white hover:border-green-600 text-black font-semibold w-12 h-12 flex justify-center items-center rounded-full">
+              <PlusCircle />
+            </button>
+          </h1>
+          <span className="absolute h-5 w-[60%] rounded-xl bg-slate-50 opacity-50 blur-lg top-10 backdrop-blur-sm  left-0 transform -translate-y-1/2"></span>
         </div>
-      </section>
-)
-}
+        <p className="text-xl mb-8 text-white">
+          Developing reliable mobile and web experiences.{" "}
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
