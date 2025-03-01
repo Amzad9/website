951092a6ce5ab70562d37f85a3302e8adfdb2171
mohd shadab saifi <shadabsaifi749@gmail.com>
2025-03-01 18:12:02 +0530

    add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,84 +1,182 @@
 import Image from "next/image";
 import Logo from "@/assets/logof.svg";
 import Link from "next/link";
-import { Map } from 'lucide-react';
+import { motion } from "framer-motion";
+import styles from "./footer.module.scss";
 
-const Footer = ()=>{
- return(
-    <footer className="bg-[#000000] text-white py-12 relative z-10">
-        <div className="container mx-auto px-6">
-          <div className="grid md:grid-cols-12 gap-8 items-start">
-            <div className="col-span-4 text-start">
-<Link href="/" className="logo">
-              <Image
-                src={Logo}
-                alt="weblibron logo"
-                width={150}
-              />
-            </Link>
-              <Link href="#">Icon</Link>
+const SocialMediaList = [
+  {
+    icon: "instagram",
+    url: "",
+  },
+  {
+    icon: "twitter",
+    url: "",
+  },
+  {
+    icon: "linkedin",
+    url: "",
+  },
+];
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-[#000000] text-white pt-12 relative z-10">
+      <div className="container mx-auto px-6">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+          {/* Logo and Description */}
+          <div className="col-start-1 col-end-3">
+            <div className="flex items-center space-x-2 mb-4">
+              <Link href="/" className="logo">
+                <Image src={Logo} alt="weblibron logo" width={150} />
+              </Link>
             </div>
-            <div className="col-span-3">
-              {/* <h3 className="text-lg font-semibold mb-4">Connect</h3> */}
-              <div className="flex flex-col space-y-5 items-start">
-                <a href="#" className="text-gray-400 text-sm font-inter hover:text-white">
+            <p className="text-sm w-2/3">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+            </p>
+            {/* Social Media Icons */}
+            <ul className="flex w-full border-t-4 pt-5 pb-5 border-white border-opacity-5 gap-5 items-center">
+              {SocialMediaList.map((item, key) => (
+                <motion.li
+                  whileHover={{
+                    scale: 1.1, // Slightly less dramatic scaling
+                    rotate: -3, // Subtle rotation for a smoother effect
+                    transition: {
+                      duration: 0.4, // Slightly longer duration for smoothness
+                      ease: [0.16, 1, 0.3, 1], // Custom cubic-bezier for smooth ease-out
+                    },
+                  }}
+                  whileTap={{
+                    scale: 0.9, // Less aggressive scaling on tap
+                    transition: {
+                      duration: 0.2, // Quick response for tap interaction
+                      ease: "easeInOut", // Smooth ease-in-out for tap
+                    },
+                  }}
+                  key={key}
+                >
+                  <Link href={item.url} target="_blank">
+                    <i className={styles[item.icon]}></i>
+                  </Link>
+                </motion.li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Company Section */}
+          <div className="">
+            <h4 className="text-lg font-semibold text-white mb-2">Connect</h4>
+            <ul className="space-y-2 text-sm">
+              <li>
+                <a
+                  href="#home"
+                  className="hover:text-primary hover:ps-1 hover:underline duration-300 ease-in-out"
+                >
                   Home
                 </a>
-                <a href="#"className="text-gray-400 text-sm font-inter hover:text-white">
-                  About
+              </li>
+              <li>
+                <a
+                  href="#about"
+                  className="hover:text-primary hover:ps-1 hover:underline duration-300 ease-in-out"
+                >
+                  About Us
                 </a>
-                <a href="#" className="text-gray-400 text-sm font-inter hover:text-white">
-                  Services
+              </li>
+              <li>
+                <a
+                  href="#services"
+                  className="hover:text-primary hover:ps-1 hover:underline duration-300 ease-in-out"
+                >
+                  Service
                 </a>
-                <a href="#" className="text-gray-400 text-sm font-inter hover:text-white">
-                  Portfolio
+              </li>
+              <li>
+                <a
+                  href="#technology"
+                  className="hover:text-primary hover:ps-1 hover:underline duration-300 ease-in-out"
+                >
+                  Technology
                 </a>
-                <a href="#" className="text-gray-400 text-sm font-inter hover:text-white">
-                  Contact Us
-                </a>
-              </div>
-            </div>
-            <div className="col-span-5"> 
-               <div className="flex items-start mb-4">
-                 <div className="bg-gray-800 w-12 h-12 rounded-full flex items-center justify-center me-2"><Map /></div>
-                 <div>
-                   <h6 className="text-white font-semibold font-poppins mb-1">Dubai Office</h6>
-               <p className="text-gray-400 mb-3 font-inter">
-                115, Al Ahmadi Building, Al Qusais, Dubai, U.A.E
-              </p>
-                 </div>
-              </div>
-              <div className="flex items-start mb-4">
-                 <div className="bg-gray-800 w-12 h-12 rounded-full flex items-center justify-center me-2"><Map /></div>
-                 <div>
-  <h6 className="text-white font-semibold font-poppins">India Office (Ajnara Gen X, Gaziabad)</h6>
-               <p className="text-gray-400 mb-3 font-inter">
-                Ajnara Gen X, Gaziabad, Uttar Pradesh, India
-              </p>
-</div>
-              </div>
-              <div className="flex items-start mb-4">
-                  <div className="bg-gray-800 w-12 h-12 rounded-full flex items-center justify-center me-2"><Map /></div>
-                 <div>
- <h6 className="text-white font-semibold font-poppins mb-1">India Office (Prayagraj)</h6>
-               <p className="text-gray-400 font-inter mb-3">
-                Kareli, Prayagraj, Uttar Pradesh, India
-              </p>
-</div>
-              </div>
-               
-             
-              
-            </div>
+              </li>
+            </ul>
           </div>
-         <div className="text-left w-full border-t border-t-gray-800">
-         <p className="text-center text-gray-400 mt-8">
-            Copyright © {new Date().getFullYear()} Weblibron. All rights reserved.
-         </p>
-         </div>
-        </div>
-      </footer>
-)
-}
 
-export default Footer
+          {/* Support Section */}
+          <div className="">
+            <h4 className="text-lg font-semibold text-white mb-2">Support</h4>
+            <ul className="space-y-2 text-sm">
+              <li>
+                <a
+                  href="#support"
+                  className="hover:text-primary hover:ps-1 hover:underline duration-300 ease-in-out"
+                >
+                  Customer Support
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#terms"
+                  className="hover:text-primary hover:ps-1 hover:underline duration-300 ease-in-out"
+                >
+                  Terms & Conditions
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#privacy"
+                  className="hover:text-primary hover:ps-1 hover:underline duration-300 ease-in-out"
+                >
+                  Privacy Policy
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Location Section */}
+          <div className="col-span-2 col-end-7">
+            <h4 className="text-lg font-semibold text-white mb-2">Location</h4>
+            <ul className="space-y-2 text-sm">
+              <li>
+                <h5 className="text-base text-white">Dubai Office</h5>
+                <a
+                  href="#dubai"
+                  className="hover:text-primary hover:underline duration-300 ease-in-out text-xs font-medium text-gray-400"
+                >
+                  115, Al Ahmadi Building, Al Qusais, Dubai, U.A.E
+                </a>
+              </li>
+              <li>
+                <h5 className="text-base text-white">India Office</h5>
+                <a
+                  href="#india1"
+                  className="hover:text-primary hover:underline duration-300 ease-in-out text-xs font-medium text-gray-400"
+                >
+                  Ajnara Gen X, Gaziabad, Uttar Pradesh, India
+                </a>
+              </li>
+              <li>
+                <h5 className="text-base text-white">India Office</h5>
+                <a
+                  href="#india2"
+                  className="hover:text-primary hover:underline duration-300 ease-in-out text-xs font-medium text-gray-400"
+                >
+                  Kareli, Prayagraj, Uttar Pradesh, India
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="text-left w-full border-t border-t-gray-800 mt-8">
+          <p className="text-center text-gray-400 py-5">
+            Copyright © {new Date().getFullYear()} Weblibron. All rights
+            reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
