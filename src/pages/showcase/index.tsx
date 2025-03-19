067c2af0a8445ae50d0be661b260938d711e45fa
amzad949 <amzad.khan@ftachdata.com>
2025-03-19 22:12:50 +0530

    add content

--- src/pages/showcase/index.tsx
+++ src/pages/showcase/index.tsx
@@ -1,15 +1,22 @@
 // import Header from "@/components/header/Header"
 import Showcase from "@/components/showcase/showcase"
+import Head from "next/head"
 // import Logo from "@/assets/logoBlack.png";
 
 
 
 const ShowCase = () => {
 return(
+<>
+<Head>
+<meta name="robots" content="index, follow" />
+<title>Weblibron - Portfolio</title>  
+</Head>
 <div className="bg-white container xl:container mx-auto">
-{/* <Header logo={Logo} /> */}
 <Showcase />
 </div>
+</>
+
 )
 }
 
