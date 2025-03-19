0c52cc8ac4cc82789b08dd665c0de71f339fa0ad
amzad949 <amzad.khan@ftachdata.com>
2025-03-19 22:21:25 +0530

    add content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <meta name="robots" content="index, follow" />
       <body className="antialiased">
         <Main />
         <NextScript />
