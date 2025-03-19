8d55c0034b18bec216c7743914dcc8c6b0646843
amzad949 <amzad.khan@ftachdata.com>
2025-03-19 12:03:12 +0530

    add apollo screipt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,14 @@ import LogoWhite from "@/assets/whiteLogo.png";
 import { useRouter } from "next/router";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import Contact from "@/components/contact/Contact";
+
+declare global {
+  interface Window {
+    trackingFunctions: {
+      onLoad: (config: { appId: string }) => void;
+    };
+  }
+}
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const logo = router.pathname === "/" ? LogoWhite : LogoWhite;
@@ -18,6 +26,20 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/wl.jpg" />
         <title>Weblibron</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script dangerouslySetInnerHTML={{ __html: `
+          function initApollo() {
+            var n = Math.random().toString(36).substring(7),
+                o = document.createElement("script");
+            o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
+            o.async = true;
+            o.defer = true;
+            o.onload = function() {
+              window.trackingFunctions.onLoad({ appId: "67d8fae9d38b9d0011ee8a51" });
+            };
+            document.head.appendChild(o);
+          }
+          initApollo();
+        ` }} />
       </Head>
       <SpeedInsights />
       <Header logo={logo} />
